695d459c04bbcea9e05b1dee10ca9a69fa9dd736
c.josso <c.josso@rennesmetropole.fr>
2026-03-11 17:45:52 +0100

    Add metrics to int + percentage check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, hash_map}, string};
+use std::{collections::HashMap, usize};
 use clap::Parser;
 use ureq::{self, Error};
 use regex::Regex;
@@ -19,6 +19,36 @@ struct Args {
     critical: u8,
 }
 
+// Convert scientific exp notation to int : 1.2345+e5 -> 123450
+fn convert_scientific_notation_to_int(numeric_string: &String) -> u64 {
+
+    // Initialize return
+    let mut return_int: u64 = 0;
+
+    // Cut prometheus style metrics 1.2334e+08
+    let splitted_string: Vec<&str> = numeric_string.split("e+").collect();
+
+    // Check if it was raw value or e+ style
+    if splitted_string.len() == 2 {
+
+        // Manipulate metrics to int:  1.2345e08 -> 12345 / 8
+        let raw_value = splitted_string[0].replace(".", "");
+        let exp_value = splitted_string[1].parse::<u8>().unwrap();
+
+        // fill with zeroes if needed
+        if raw_value.len() <= usize::from(exp_value) {
+
+            let fill_number = &exp_value + 1 - raw_value.len() as u8;
+            return_int = raw_value.parse::<u64>().unwrap() * u64::pow(10, fill_number as u32) ;
+
+        } else if raw_value.len() == usize::from(exp_value) + 1 {
+            return_int = raw_value.parse::<u64>().unwrap();
+        }
+    }
+
+    return return_int;
+}
+
 fn main() -> Result<(), Error> {
     // Parser Args
     let args: Args = Args::parse();
@@ -26,6 +56,15 @@ fn main() -> Result<(), Error> {
     let warning: u8 = args.warning;
     let critical: u8 = args.critical;
 
+    // placeholder for criticals and warnings
+    let mut critical_list: Vec<String> = [].to_vec();
+    let mut warning_list: Vec<String> = [].to_vec();
+
+    // placeholder to send result
+    let mut resut_line: String = "".to_string();
+    let mut project_line: String = "".to_string();
+
+    
     // Definition of the http get
     let body = ureq::get(url)
         .call()?
@@ -53,7 +92,7 @@ fn main() -> Result<(), Error> {
         let subject: String = project_parser["subject"].to_string();
         let value: String = project_parser["value"].to_string();
 
-        // Building of the hashmap
+        // Building the hashmap
         if projects.contains_key(project_name.as_str()) {
 
             let temp_hash = projects.get_mut(&project_name).unwrap();
@@ -67,7 +106,34 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    println!("{:?}",projects);
+    // Creating output from hashmap
+    for (hash_project_name, hash_metrics) in projects {
 
+        // Convert metrics to int
+        let used_quota: u64 = convert_scientific_notation_to_int(&hash_metrics["project_quota_usage_byte"]);
+        let total_quota: u64 = convert_scientific_notation_to_int(&hash_metrics["project_quota_byte"]);
+
+        // Compute usage percentage
+        let percentage_quota: u64 = used_quota * 100 / total_quota;
+        
+        // Create NRPE style string
+        project_line += &format!("{}={};{warning};{critical};0;100", &hash_project_name, &percentage_quota);
+
+        // Check if we need to alert as crit or warn
+        if percentage_quota >= critical as u64 {
+
+            critical_list.push(hash_project_name);
+
+        } else if percentage_quota >= warning as u64 {
+
+            warning_list.push(hash_project_name);
+
+        }
+
+    }
+
+
+
+    println!("{project_line}");
     Ok(())
 }
